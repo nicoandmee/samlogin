daf5c7da71ed0bc1a4aff9a43de6f22689943421
Nico Kokonas <nico@nicomee.com>
2022-05-17 13:37:44 -0600

    index package-lock

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
 // test
 // ************
-const AWS              = require('aws-sdk');
-const fs               = require('fs');
-const homedir          = require('os').homedir();
-const path             = require('path');
-const puppeteer        = require('puppeteer');
-const yaml             = require('js-yaml');
-const { URL }          = require('url');
-const { parse }        = require('querystring');
-const LibSaml          = require('libsaml');
-const locatePath       = require('locate-path');
-const parseArgs        = require('minimist');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const homedir = require('os').homedir();
+const path = require('path');
+const puppeteer = require('puppeteer');
+const yaml = require('js-yaml');
+const { URL } = require('url');
+const { parse } = require('querystring');
+const LibSaml = require('libsaml');
+const locatePath = require('locate-path');
+const parseArgs = require('minimist');
 const { stripIndents } = require('common-tags');
-const winston          = require('winston');
+const winston = require('winston');
 
-const sts              = require('./sts.js');
-
+const sts = require('./sts.js');
 
 // *********
 // Functions
@@ -24,10 +23,10 @@ const sts              = require('./sts.js');
 async function createCredentialBlock(identity) {
   const { accountNumber, roleName, credentials } = await identity;
 
-  return (stripIndents`[${accountNumber}-${roleName}]
+  return stripIndents`[${accountNumber}-${roleName}]
            aws_access_key_id = ${credentials.AccessKeyId}
            aws_secret_access_key = ${credentials.SecretAccessKey}
-           aws_session_token = ${credentials.SessionToken}`);
+           aws_session_token = ${credentials.SessionToken}`;
 }
 
 function outputDocAsDownload(doc) {
@@ -41,106 +40,91 @@ async function buildDocument(doc, credBlock) {
 
 async function substituteAccountAlias(credBlock, config) {
   if (config.AccountAliases) {
-    return config.AccountAliases
-      .reduce((acc, alias) => {
-        const re = new RegExp(`\\[${alias.AccountNumber}-(.*)\\]`);
-        return acc.replace(re, `[${alias.Alias}-$1]`);
-      }, (await credBlock));
+    return config.AccountAliases.reduce((acc, alias) => {
+      const re = new RegExp(`\\[${alias.AccountNumber}-(.*)\\]`);
+      return acc.replace(re, `[${alias.Alias}-$1]`);
+    }, await credBlock);
   }
 
   return credBlock;
 }
 
 function onBeforeRequestEvent(details, config, logger) {
-  const roleAttributeName  = 'https://aws.amazon.com/SAML/Attributes/Role';
+  const roleAttributeName = 'https://aws.amazon.com/SAML/Attributes/Role';
 
   /* eslint no-underscore-dangle: ["error", { "allow": ["_postData"] }] */
   const samlResponseBase64 = unescape(parse(details._postData).SAMLResponse);
 
   const STS = new AWS.STS({
-    apiVersion:  '2014-10-01',
+    apiVersion: '2014-10-01',
     httpOptions: {
-                   proxy: process.env.https_proxy
-                          || process.env.HTTPS_PROXY
-                          || '',
-                 },
+      proxy: process.env.https_proxy || process.env.HTTPS_PROXY || '',
+    },
   });
 
   new LibSaml(samlResponseBase64)
     .getAttribute(roleAttributeName)
-    .map(role => sts.assumeRole(config, logger, STS, role, samlResponseBase64))
-    .filter(x => x != null) // filter roles which could not be assumed.
-    .map(identity => createCredentialBlock(identity))
-    .map(credBlock => substituteAccountAlias(credBlock, config))
+    .map((role) =>
+      sts.assumeRole(config, logger, STS, role, samlResponseBase64)
+    )
+    .filter((x) => x != null) // filter roles which could not be assumed.
+    .map((identity) => createCredentialBlock(identity))
+    .map((credBlock) => substituteAccountAlias(credBlock, config))
     .reduce((doc, credBlock) => buildDocument(doc, credBlock), '')
-    .then(doc => outputDocAsDownload(doc))
-    .catch((err) => { throw (err); });
+    .then((doc) => outputDocAsDownload(doc))
+    .catch((err) => {
+      throw err;
+    });
 }
-
 
 async function locateConfig(appName, argv) {
-  const localAppData = process.env.LocalAppData
-                       || path.join(
-                                     process.env.HOME,
-                                     'AppData',
-                                     'Local',
-                                   );
+  const localAppData =
+    process.env.LocalAppData || path.join(process.env.HOME, 'AppData', 'Local');
 
-  const xdgConfigHome = process.env.XDG_CONFIG_HOME
-                        || path.join(
-                                      process.env.HOME,
-                                      '.config',
-                                    );
+  const xdgConfigHome =
+    process.env.XDG_CONFIG_HOME || path.join(process.env.HOME, '.config');
 
   const preferencesPath = path.join(
-                                     process.env.HOME,
-                                     'Library',
-                                     'Application Support',
-                                   );
+    process.env.HOME,
+    'Library',
+    'Application Support'
+  );
 
-  return argv.config || locatePath([
-    path.join(localAppData, appName, 'config.yaml'),
-    path.join(xdgConfigHome, appName, 'config.yaml'),
-    path.join(preferencesPath, appName, 'config.yaml'),
-    path.join(process.env.HOME, `.${appName}.yaml`),
-  ]);
+  return (
+    argv.config ||
+    locatePath([
+      path.join(localAppData, appName, 'config.yaml'),
+      path.join(xdgConfigHome, appName, 'config.yaml'),
+      path.join(preferencesPath, appName, 'config.yaml'),
+      path.join(process.env.HOME, `.${appName}.yaml`),
+    ])
+  );
 }
 
-
 async function locateDataPath(appName) {
-  const localAppData = process.env.LocalAppData
-                       || path.join(
-                                     process.env.HOME,
-                                     'AppData',
-                                     'Local',
-                                   );
+  const localAppData =
+    process.env.LocalAppData || path.join(process.env.HOME, 'AppData', 'Local');
 
-  const xdgDataHome = process.env.XDG_DATA_HOME
-                      || path.join(
-                                    process.env.HOME,
-                                    '.local',
-                                    'share',
-                                  );
+  const xdgDataHome =
+    process.env.XDG_DATA_HOME || path.join(process.env.HOME, '.local', 'share');
 
   const appSupportPath = path.join(
-                                    process.env.HOME,
-                                    'Library',
-                                    'Application Support',
-                                  );
+    process.env.HOME,
+    'Library',
+    'Application Support'
+  );
 
   const dataHome = await locatePath([
-                                     localAppData,
-                                     xdgDataHome,
-                                     appSupportPath,
-                                     process.env.HOME,
-                                   ]);
+    localAppData,
+    xdgDataHome,
+    appSupportPath,
+    process.env.HOME,
+  ]);
 
   return path.join(
-                    dataHome,
-                    (dataHome === process.env.HOME
-                      ? `.${appName}`
-                      : appName),
-                  );
+    dataHome,
+    dataHome === process.env.HOME ? `.${appName}` : appName
+  );
 }
 
 // ****************
@@ -148,21 +132,16 @@ async function locateDataPath(appName) {
 // ****************
 (async () => {
   const appName = 'samlogin';
-  const argv    = parseArgs(
-                             process.argv.slice(2),
-                             {
-                               config: 'string',
-                               debug:  'boolean',
-                             },
-                           );
+  const argv = parseArgs(process.argv.slice(2), {
+    config: 'string',
+    debug: 'boolean',
+  });
 
   const logger = winston.createLogger({
-    level:       argv.debug ? 'debug' : 'info',
-    format:      winston.format.simple(),
+    level: argv.debug ? 'debug' : 'info',
+    format: winston.format.simple(),
     exitOnError: true,
-    transports:  [
-                   new winston.transports.Console(),
-                 ],
+    transports: [new winston.transports.Console()],
   });
 
   if (process.env.HOME === undefined) {
@@ -172,23 +151,18 @@ async function locateDataPath(appName) {
 
   const fmtConfigNotFound = 'No configuration file could be found.';
 
-  const configPath = await locateConfig(appName, argv)
-                     || (logger.error(fmtConfigNotFound) && process.exit());
+  const configPath =
+    (await locateConfig(appName, argv)) ||
+    (logger.error(fmtConfigNotFound) && process.exit());
 
-  const config = yaml.safeLoad(fs.readFileSync(
-    configPath,
-    'utf8',
-  ));
+  const config = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
   const authUrl = config.AuthUrl;
   const samlUrl = config.samlUrl || 'https://signin.aws.amazon.com/saml';
 
   const browser = await puppeteer.launch({
     executablePath: `/usr/bin/google-chrome-stable`,
-    headless:    false,
-    userDataDir: path.join(
-                            await locateDataPath(appName),
-                            'Chrome',
-                          ),
+    headless: false,
+    userDataDir: path.join(await locateDataPath(appName), 'Chrome'),
   });
 
   const page = await browser.newPage();
@@ -196,6 +170,7 @@ async function locateDataPath(appName) {
   await page.setRequestInterception(true);
 
   page.on('request', (interceptedRequest) => {
+    // console.log(interceptedRequest.url());
     interceptedRequest.continue();
 
     if (interceptedRequest.url() === samlUrl) {
@@ -206,4 +181,4 @@ async function locateDataPath(appName) {
   await page.goto(new URL(authUrl).href, { timeout: 0 });
   await page.waitForRequest(samlUrl, { timeout: 0 });
   browser.close();
-})().catch(console.error)
+})().catch(console.error);
